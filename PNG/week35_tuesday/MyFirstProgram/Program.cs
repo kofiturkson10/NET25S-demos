7906c008c4ca611cadeb72bad5d55722ef67368e
Sebastian Degerman <sebbed89@gmail.com>
2025-08-22 16:00:34 +0200

    rename existing stub demo, add another one

---
+++ PNG/week35_tuesday/MyFirstProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hej och välkommen till C#!");
+
+        Console.WriteLine("Mata in ditt namn:");
+        string? namn = Console.ReadLine();
+
+        Console.WriteLine("Hej " + namn + ", kul att du är här!");
+    }
+}
