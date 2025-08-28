7017eb7517edfac8211a1f1865894f8035418140
Sebastian Degerman <sebbed89@gmail.com>
2025-08-28 15:22:31 +0200

    add week 35 thursday demos

---
+++ PNG/week35_thursday/LogicDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // if + logical operators
+        int temperature = 9;
+        string season = "winter";
+        string headwear;
+
+        if (temperature <= 8 && season == "winter" || temperature >= 8 && season == "summer")
+        {
+            headwear = "Beanie";
+        }
+        else
+        {
+            headwear = "Cap";
+        }
+
+        Console.WriteLine("It is " + temperature + "°C and " + season + ", wear: " + headwear);
+
+        // if - else if - else
+        int busDelay = 0;
+        if (busDelay > 5)
+        {
+            Console.WriteLine("The bus is late, take the subway!");
+        }
+        else if (busDelay > 0)
+        {
+            Console.WriteLine("The bus is a little late, wait for it.");
+        }
+        else
+        {
+            Console.WriteLine("The bus is on time - hooray!");
+        }
+
+        // switch
+        int number = 2;
+        switch (number)
+        {
+            case 0:
+                Console.WriteLine("The number is: " + number);
+                break;
+            case 1:
+                Console.WriteLine("The number is: " + number);
+                break;
+            case 2:
+                Console.WriteLine("The number is: " + number);
+                break;
+            default:
+                Console.WriteLine("The number is something else!");
+                break;
+        }
+
+        // ternary operator
+        int choice = -1;
+        string message = choice != 1 ? "not equal to 1" : "equal to 1";
+        Console.WriteLine($"Value is {message}");
+
+        // scope
+        int testNumberOne = 10;
+        if (testNumberOne > 5)
+        {
+            int testNumberTwo = 20;
+            Console.WriteLine($"Inside if: testNumber = {testNumberOne}, testNumberTwo = {testNumberTwo}");
+        }
+    }
+}
---
+++ PNG/week35_thursday/VariablesDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Exempel på hur det syns är att skriva kommandot:
+        // dotnet run -- one two three four
+        Console.WriteLine(string.Join(", ", args));
+
+        // Exempel på olika datatyper
+        int number = 10;
+        double temp = 36.6;
+        string name = "Lisa";
+        bool hasLicense = true;
+        const int testNumber = 123;
+        // Nedan går ej pga const
+        // testNumber = 456;
+
+        Console.WriteLine("Heltal: " + number);
+        Console.WriteLine("Temperatur: " + temp);
+        Console.WriteLine("Namn: " + name);
+        Console.WriteLine("Har körkort? " + hasLicense);
+        Console.WriteLine(testNumber);
+
+        // Operatorer
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        int product = num1 * num2;
+
+        Console.WriteLine("num1 + num2 = " + sum);
+        Console.WriteLine("num1 * num2 = " + product);
+
+        // Typkonvertering (string → int)
+        Console.WriteLine("Mata in ett heltal: ");
+        var userInput = Console.ReadLine();
+        int convertedNumber = int.Parse(userInput);
+        Console.WriteLine("Du skrev: " + convertedNumber);
+
+        // Inkrementering och dekrementering
+        int counter = 0;
+        counter++;
+        Console.WriteLine("counter är " + counter);
+        counter--;
+        Console.WriteLine("counter är " + counter);
+    }
+}
