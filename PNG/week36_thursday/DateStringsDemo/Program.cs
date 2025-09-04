10cc199d27d1f8e9e84993921471bf1e59de9305
Sebastian Degerman <sebbed89@gmail.com>
2025-09-04 15:33:17 +0200

    fix code in DateStringsDemo/Program.cs

--- PNG/week36_thursday/DateStringsDemo/Program.cs
+++ PNG/week36_thursday/DateStringsDemo/Program.cs
@@ -2,11 +2,7 @@
 {
     static void Main()
     {
-        // Helper.DateDemo();
-        // Helper.StringDemo();
-
-        double number = 7.4;
-        var invoiceAmount = number * 1.25;
-        Console.WriteLine($"Faktura beloppet är {invoiceAmount}");
+        Helper.DateDemo();
+        Helper.StringDemo();
     }
 }
